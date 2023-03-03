5b3290ce1b412b3dde683d881f49e2b68fbeaea4
sophiabob <117973894+sophiabob@users.noreply.github.com>
2023-03-03 11:38:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class main {
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        JFrame f = new JFrame("");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MyPanel myPanel = new MyPanel();
+        myPanel.repaint();
+        f.add(myPanel);
+        f.pack();
+        f.setVisible(true);
+    }
 }
